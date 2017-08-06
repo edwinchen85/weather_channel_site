f0f9e33d19d656beee7c35fc8b867015b0e38fc0
edwinchen85 <edwinchen85@gmail.com>
2017-08-06 10:22:34 +0800

    Define loadWeather function

--- js/main.js
+++ js/main.js
@@ -5,9 +5,18 @@ var weatherForecast = new XMLHttpRequest();
 var cObj;
 var fObj;
 
-weatherConditions.open('GET', 'http://api.wunderground.com/api/9885a4395f919785/conditions/q/84653.json', true);
-weatherConditions.responseType = 'text';
-weatherConditions.send();
+function loadWeather() {
+
+  weatherConditions.open('GET', 'http://api.wunderground.com/api/9885a4395f919785/conditions/q/84653.json', true);
+  weatherConditions.responseType = 'text';
+  weatherConditions.send();
+
+  weatherForecast.open('GET', 'http://api.wunderground.com/api/9885a4395f919785/forecast/q/84653.json', true);
+  weatherForecast.responseType = 'text';
+  weatherForecast.send();
+
+}
+
 
 weatherConditions.onload = function() {
   if (weatherConditions.status === 200){
@@ -21,10 +30,6 @@ weatherConditions.onload = function() {
 };
 
 
-
-weatherForecast.open('GET', 'http://api.wunderground.com/api/9885a4395f919785/forecast/q/84653.json', true);
-weatherForecast.responseType = 'text';
-weatherForecast.send();
 
 weatherForecast.onload = function() {
   if (weatherForecast.status === 200){
