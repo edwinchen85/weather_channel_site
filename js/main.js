9317be25768fb0966d0251a3d54aca6626cb160f
edwinchen85 <edwinchen85@gmail.com>
2017-08-06 09:58:17 +0800

    Set up the stage for AJAX call for conditions and forecast

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var weatherForecast = new XMLHttpRequest();
 var cObj;
 var fObj;
 
-weatherConditions.open('', '', true);
+weatherConditions.open('GET', 'http://api.wunderground.com/api/9885a4395f919785/conditions/q/84653.json', true);
 weatherConditions.responseType = 'text';
 weatherConditions.send();
 
@@ -20,7 +20,7 @@ weatherConditions.onload = function() {
 
 
 
-weatherForecast.open('', '', true);
+weatherForecast.open('GET', 'http://api.wunderground.com/api/9885a4395f919785/forecast/q/84653.json', true);
 weatherForecast.responseType = 'text';
 weatherForecast.send();
 
